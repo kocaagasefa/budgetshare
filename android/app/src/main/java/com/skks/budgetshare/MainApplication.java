e396dcdc11553963988d3684102be6c18b152e37
sefakocaaga <sefa@halici.com.tr>
2019-08-01 21:33:47 +0300

    firestore added

--- android/app/src/main/java/com/skks/budgetshare/MainApplication.java
+++ android/app/src/main/java/com/skks/budgetshare/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -28,7 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
           new MainReactPackage(),
             new RNFirebasePackage(),
             new RNGestureHandlerPackage(),
-            new RNFirebaseAuthPackage() 
+            new RNFirebaseAuthPackage(),
+            new RNFirebaseFirestorePackage() 
       );
     }
 
